f3f57c1d15d42bcb46e2b22e95a52891d6b19435
Matyáš Strelec <matyasstrelec@gmail.com>
2023-03-25 16:47:51 +0100

    add basic structure for part 2

---
+++ create_objects.sql
@@ -0,0 +1,34 @@
+-- IDS projekt 2022
+-- Cast 2 - SQL skript pro vytvoření objektů schématu databáze
+-- Autor: xmrkva04 & xstrel03
+
+-- Drop:
+
+DROP TABLE "spolecnost";
+DROP TABLE "letadlo";
+DROP TABLE "let";
+DROP TABLE "letovy_rezim";
+DROP TABLE "letiste";
+DROP TABLE "kosik";
+DROP TABLE "datum";
+DROP TABLE "zakaznik";
+
+-- Create:
+
+CREATE TABLE "spolecnost" ();
+
+CREATE TABLE "letadlo" ();
+
+CREATE TABLE "let" ();
+
+CREATE TABLE "letovy_rezim" ();
+
+CREATE TABLE "letiste" ();
+
+CREATE TABLE "kosik" ();
+
+CREATE TABLE "datum" ();
+
+CREATE TABLE "zakaznik" ();
+
+-- Insert:
