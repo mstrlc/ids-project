1448325e1e1b4e54ee03c1d789c2a94b207b731c
Matyáš Strelec <matyasstrelec@gmail.com>
2023-03-26 12:09:08 +0200

    edit and add some inserts

--- create_objects.sql
+++ create_objects.sql
@@ -8,8 +8,7 @@ ALTER TABLE "letadlo" DROP CONSTRAINT "fk_letadlo_spolecnost_id";
 ALTER TABLE "letovy_rezim" DROP CONSTRAINT "fk_letovy_rezim_misto_priletu";
 ALTER TABLE "letovy_rezim" DROP CONSTRAINT "fk_letovy_rezim_misto_odletu";
 ALTER TABLE "let" DROP CONSTRAINT "fk_let_letovy_rezim_letu";
-ALTER TABLE "letadlo_leta_let" DROP CONSTRAINT "fk_letadlo_leta_let_letadlo_seriove_cislo";
-ALTER TABLE "letadlo_leta_let" DROP CONSTRAINT "fk_letadlo_leta_let_let_id";
+ALTER TABLE "let" DROP CONSTRAINT "fk_let_letadlo_seriove_cislo";
 ALTER TABLE "letovy_rezim_aktivni_v_datum" DROP CONSTRAINT "fk_letovy_rezim_aktivni_v_datum_letovy_rezim_id";
 ALTER TABLE "letovy_rezim_aktivni_v_datum" DROP CONSTRAINT "fk_letovy_rezim_aktivni_v_datum_datum_datum";
 ALTER TABLE "kosik" DROP CONSTRAINT "fk_kosik_zakaznik_rezervoval_id";
@@ -24,7 +23,6 @@ DROP TABLE "letadlo";
 DROP TABLE "letiste";
 DROP TABLE "letovy_rezim";
 DROP TABLE "let";
-DROP TABLE "letadlo_leta_let";
 DROP TABLE "datum";
 DROP TABLE "zakaznik";
 DROP TABLE "letovy_rezim_aktivni_v_datum";
@@ -36,8 +34,9 @@ DROP TABLE "kosik_rezervuje_let";
 
 CREATE TABLE "spolecnost" (
     "id" VARCHAR(3) NOT NULL PRIMARY KEY,
-        CHECK ( LENGTH("id") = 3 ),
-    "jmeno" VARCHAR(100) DEFAULT NULL
+        CHECK ( LENGTH("id") <= 3 ),
+        CHECK ( LENGTH("id") >= 2 ),
+    "nazev" VARCHAR(100) DEFAULT NULL
 );
 
 -- Oproti ER diagramu je seriove cislo letadla unikatni v ramci celeho systemu.
@@ -65,9 +64,9 @@ CREATE TABLE "letiste" (
 -- Oproti ER diagramu je misto priletu a odletu presunuto do letoveho rezimu,
 -- letovy rezim dodrzuje 0..n letu. 
 CREATE TABLE "letovy_rezim" (
-    "id" VARCHAR(7) NOT NULL PRIMARY KEY,
-    "pravidelny_cas_priletu" TIMESTAMP NOT NULL,
-    "pravidelny_cas_odletu" TIMESTAMP NOT NULL,
+    "id" INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
+    "pravidelny_cas_priletu" VARCHAR(5) NOT NULL,
+    "pravidelny_cas_odletu" VARCHAR(5) NOT NULL,
     "misto_priletu" VARCHAR(3) NOT NULL,
     "misto_odletu" VARCHAR(3) NOT NULL,
     
@@ -87,10 +86,10 @@ CREATE TABLE "letovy_rezim" (
 -- na letu je nasazeno 1 letadlo.
 CREATE TABLE "let" (
     "id" INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
-    "skutecny_cas_odletu" TIMESTAMP DEFAULT NULL,
-    "skutecny_cas_pristani" TIMESTAMP DEFAULT NULL,
+    "skutecny_cas_odletu" VARCHAR(5) DEFAULT NULL,
+    "skutecny_cas_pristani" VARCHAR(5) DEFAULT NULL,
     "poznamka" VARCHAR(100) DEFAULT NULL,
-    "letovy_rezim_letu" VARCHAR(7) NOT NULL,
+    "letovy_rezim_letu" INT NOT NULL,
     "letadlo_seriove_cislo" INT NOT NULL,
 
     CONSTRAINT "fk_let_letovy_rezim_letu"
@@ -109,7 +108,7 @@ CREATE TABLE "datum" (
 );
 
 CREATE TABLE "letovy_rezim_aktivni_v_datum" (
-    "letovy_rezim_id" VARCHAR(7) NOT NULL,
+    "letovy_rezim_id" INT NOT NULL,
     "datum_datum" DATE NOT NULL,
 
     CONSTRAINT "pk_letovy_rezim_aktivni_v_datum"
@@ -126,21 +125,24 @@ CREATE TABLE "letovy_rezim_aktivni_v_datum" (
         ON DELETE SET NULL
 );
 
+-- Oproti ER diagramu je pro zakazniky bez RC pouzito cislo OP
 CREATE TABLE "zakaznik" (
     "id" INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
     "narodnost" VARCHAR(2) NOT NULL
         CHECK ( LENGTH("narodnost") = 2 ),
     "jmeno" VARCHAR(100) NOT NULL,
-    "prijmeni" VARCHAR(100) NOT NULL
+    "prijmeni" VARCHAR(100) NOT NULL,
+    "rodne_cislo" VARCHAR(11) DEFAULT NULL,
+    "cislo_op" NUMBER(10) DEFAULT NULL
 );
 
+ALTER TABLE "zakaznik" ADD CONSTRAINT "ck_zakaznik_rodne_cislo" 
+    CHECK ( REGEXP_LIKE("rodne_cislo", '^[0-9]{6}/[0-9]{3,4}$') );
 
 CREATE TABLE "kosik" (
     "id" INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
     "celkova_cena" INT NOT NULL,
-        -- CHECK ( "celkova_cena" >= 0 ),
     "cas_expirace" TIMESTAMP NOT NULL,
-        -- CHECK ( "cas_expirace" > CURRENT_TIMESTAMP ),
     "stav_uhrady" NUMBER(1) NOT NULL,
     "zakaznik_rezervoval_id" INT NOT NULL,
     "na_datum" DATE NOT NULL,
@@ -155,6 +157,9 @@ CREATE TABLE "kosik" (
         REFERENCES "datum" ("datum")
         ON DELETE CASCADE
 );
+
+ALTER TABLE "kosik" ADD CONSTRAINT "ck_kosik_celkova_cena"
+    CHECK ( "celkova_cena" >= 0 );
 
 CREATE TABLE "kosik_pro_pasazery" (
     "kosik_id" INT NOT NULL,
@@ -191,3 +196,79 @@ CREATE TABLE "kosik_rezervuje_let" (
         REFERENCES "let" ("id")
         ON DELETE CASCADE
 );
+
+-- Insert:
+
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('PRG', 'CZ', 'Praha', 'Václav Havel Airport Prague');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('BRQ', 'CZ', 'Brno', 'Brno-Tuřany Airport');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('OSR', 'CZ', 'Ostrava', 'Ostrava Leoš Janáček Airport');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('WAW', 'PL', 'Warszawa', 'Warsaw Chopin Airport');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('BUD', 'HU', 'Budapest', 'Budapest Ferenc Liszt International Airport');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('BTS', 'SK', 'Bratislava', 'M. R. Štefánik Airport');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('VIE', 'AT', 'Wien', 'Vienna International Airport');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('MUC', 'DE', 'München', 'Munich Airport');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('LHR', 'GB', 'London', 'London Heathrow Airport');
+INSERT INTO "letiste" ("kod", "zeme", "mesto", "nazev")
+    VALUES ('CDG', 'FR', 'Paris', 'Charles de Gaulle Airport');
+
+INSERT INTO "spolecnost" ("id", "nazev")
+    VALUES ('RYR', 'Ryanair');
+INSERT INTO "spolecnost" ("id", "nazev")
+    VALUES ('EZY', 'easyJet');
+INSERT INTO "spolecnost" ("id", "nazev")
+    VALUES ('CSA', 'Czech Airlines');
+INSERT INTO "spolecnost" ("id", "nazev")
+    VALUES ('BAW', 'British Airways');
+INSERT INTO "spolecnost" ("id", "nazev")
+    VALUES ('AA', 'American Airlines');
+
+INSERT INTO "letadlo" ("seriove_cislo", "typ", "spolecnost_id")
+    VALUES (6057511439, 'Boeing 737', 'RYR');
+INSERT INTO "letadlo" ("seriove_cislo", "typ", "spolecnost_id")
+    VALUES (8532190546, 'Airbus A320', 'EZY');
+INSERT INTO "letadlo" ("seriove_cislo", "typ", "spolecnost_id")
+    VALUES (4209835716, 'Boeing 787 Dreamliner', 'RYR');
+INSERT INTO "letadlo" ("seriove_cislo", "typ", "spolecnost_id")
+    VALUES (8675493210, 'Boeing 777', 'CSA');
+INSERT INTO "letadlo" ("seriove_cislo", "typ", "spolecnost_id")
+    VALUES (1975346982, 'Airbus A320', 'EZY');
+
+INSERT INTO "letovy_rezim" ("pravidelny_cas_odletu", "pravidelny_cas_priletu", "misto_odletu", "misto_priletu")
+    VALUES ('10:00', '11:00', 'PRG', 'BUD');
+INSERT INTO "letovy_rezim" ("pravidelny_cas_odletu", "pravidelny_cas_priletu", "misto_odletu", "misto_priletu")
+    VALUES ('12:00', '13:30', 'BRQ', 'LHR');
+INSERT INTO "letovy_rezim" ("pravidelny_cas_odletu", "pravidelny_cas_priletu", "misto_odletu", "misto_priletu")
+    VALUES ('14:00', '15:00', 'OSR', 'CDG');
+INSERT INTO "letovy_rezim" ("pravidelny_cas_odletu", "pravidelny_cas_priletu", "misto_odletu", "misto_priletu")
+    VALUES ('16:00', '18:30', 'WAW', 'MUC');
+INSERT INTO "letovy_rezim" ("pravidelny_cas_odletu", "pravidelny_cas_priletu", "misto_odletu", "misto_priletu")
+    VALUES ('18:00', '20:15', 'BRQ', 'CDG');
+
+INSERT INTO "datum" ("datum")
+    VALUES (TO_DATE('2023-03-26','YYYY-MM-DD'));
+INSERT INTO "datum" ("datum")
+    VALUES (TO_DATE('2023-03-27','YYYY-MM-DD'));
+INSERT INTO "datum" ("datum")
+    VALUES (TO_DATE('2023-03-28','YYYY-MM-DD'));
+INSERT INTO "datum" ("datum")
+    VALUES (TO_DATE('2023-03-29','YYYY-MM-DD'));
+
+INSERT INTO "zakaznik" ("narodnost", "jmeno", "prijmeni", "rodne_cislo")
+    VALUES ('CZ', 'Petr', 'Novák', '745124/7890');
+INSERT INTO "zakaznik" ("narodnost", "jmeno", "prijmeni", "rodne_cislo")
+    VALUES ('CZ', 'Jan', 'Novotný', '873212/9323');
+INSERT INTO "zakaznik" ("narodnost", "jmeno", "prijmeni", "cislo_op")
+    VALUES ('PL', 'Jan', 'Kowalski', 8329647209);
+INSERT INTO "zakaznik" ("narodnost", "jmeno", "prijmeni", "cislo_op")
+    VALUES ('HU', 'János', 'Kovács', 4749234231);
+INSERT INTO "zakaznik" ("narodnost", "jmeno", "prijmeni", "cislo_op")
+    VALUES ('SK', 'Ján', 'Novák', 6385653214);
